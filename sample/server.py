5edecc52aaba7a733c92b4093a05ed8a20caf1f3
Przybyłowicz <74791570+przybylowicz0@users.noreply.github.com>
2022-03-27 23:10:42 -0400

    Update threading function for Python 3

--- sample/server.py
+++ sample/server.py
@@ -1,6 +1,7 @@
 import socket
 import select
-from threading import *
+# from threading import *
+import threading
 import sys
 
 
@@ -12,8 +13,8 @@ The second argument is the type of socket. SOCK_STREAM means that data or charac
 """
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 if len(sys.argv) != 3:
-    print("Correct usage: script, IP address, port number")
-    exit()
+	print("Correct usage: script, IP address, port number")
+	exit()
 IP_address = str(sys.argv[1])
 Port = int(sys.argv[2])
 server.bind((IP_address, Port)) 
@@ -23,46 +24,47 @@ server.listen(100)
 list_of_clients=[]
 
 def clientthread(conn, addr):
-    conn.send("Welcome to this chatroom!")
-    #sends a message to the client whose user object is conn
-    while True:
-            try:     
-                message = conn.recv(2048)    
-                if message:
-                    print("<" + addr[0] + "> " + message)
-                    message_to_send = "<" + addr[0] + "> " + message
-                    broadcast(message_to_send,conn)
-                    #prints the message and address of the user who just sent the message on the server terminal
-                else:
-                    remove(conn)
-            except:
-                continue
+	conn.send("Welcome to this chatroom!")
+	#sends a message to the client whose user object is conn
+	while True:
+			try:     
+				message = conn.recv(2048)    
+				if message:
+					print("<" + addr[0] + "> " + message)
+					message_to_send = "<" + addr[0] + "> " + message
+					broadcast(message_to_send,conn)
+					#prints the message and address of the user who just sent the message on the server terminal
+				else:
+					remove(conn)
+			except:
+				continue
 
 def broadcast(message,connection):
-    for clients in list_of_clients:
-        if clients!=connection:
-            try:
-                clients.send(message)
-            except:
-                clients.close()
-                remove(clients)
+	for clients in list_of_clients:
+		if clients!=connection:
+			try:
+				clients.send(message)
+			except:
+				clients.close()
+				remove(clients)
 
 def remove(connection):
-    if connection in list_of_clients:
-        list_of_clients.remove(connection)
+	if connection in list_of_clients:
+		list_of_clients.remove(connection)
 
 while True:
-    conn, addr = server.accept()
-    """
-    Accepts a connection request and stores two parameters, conn which is a socket object for that user, and addr which contains
-    the IP address of the client that just connected
-    """
-    list_of_clients.append(conn)
-    print(addr[0] + " connected")
-    #maintains a list of clients for ease of broadcasting a message to all available people in the chatroom
-    #Prints the address of the person who just connected
-    start_new_thread(clientthread,(conn,addr))
-    #creates and individual thread for every user that connects
+	conn, addr = server.accept()
+	"""
+	Accepts a connection request and stores two parameters, conn which is a socket object for that user, and addr which contains
+	the IP address of the client that just connected
+	"""
+	list_of_clients.append(conn)
+	print(addr[0] + " connected")
+	#maintains a list of clients for ease of broadcasting a message to all available people in the chatroom
+	#Prints the address of the person who just connected
+	#start_new_thread(clientthread,(conn,addr))
+	threading.Thread(target=clientthread, args=(conn,addr)).start()
+	#creates and individual thread for every user that connects
 
 conn.close()
 server.close()
