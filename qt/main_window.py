e503a79257e640685d85cdd631bf3acc737e6f9a
Yancey Wang <53365177+YanceyW@users.noreply.github.com>
2022-04-18 18:34:32 -0400

    repair the socket connection problem

--- qt/main_window.py
+++ qt/main_window.py
@@ -11,7 +11,7 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 import socket
 import threading
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 alias = ""
 
 ############################################################################### Main Window ###############################################################################
@@ -360,64 +360,64 @@ class Ui_Form(object):
 		self.cancel.setText(_translate("Form", "Cancel"))
 
 ########################################################################### Client #############################################################################
-
+# Create the client socket.
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # define default values
-def newConnection(hostname = "127.0.0.1", port = "55555", username = "Guest"):
-	# Create the client socket.
-	# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# Connect client to the server
-	client.connect(('127.0.0.1', 55555))
-	alias = username
-	receive(client, alias)#should remove this two argumend 'client, alias' because not match the receive()function
-	return client
+def newConnection(hostname = '127.0.0.1', port = "55555", username = "Guest"):
 
-def receive():
-    """
-    This function will allow the client to receive messages from
-    the server which other clients are trying to send them.
-    """
+    # Connect client to the server
+    client.connect((hostname,int(port)))
+    alias = username
+    #receive()#should remove this two argumend 'client, alias' because not match the receive()function
+    # return client
 
-    # While the application is running do this.
-    while True:
-        # Try and receive messages from the server
-        try:
-            # Receive the message and decode it.
-            message = client.recv(1024).decode('utf-8')
-            # If the message is the alias prompt from the server, then pass the alias.
-            if message == "Enter your alias: ":
-                # Send the alias to the server
-                client.send(alias.encode('utf-8'))
-            # Else we are just printing what the server send us as it's a regular message.
-            else:
-                print(message)
-        # If the receiving message doesn't work, do this.
-        except:
-            # Notify the client that the connection is going to be closed.
-            print("Something went wrong. Closing the connection...")
-            # Close the connection.
-            client.close()
-            # Break out of the loop.
-            break
+    def receive():
+        """
+        This function will allow the client to receive messages from
+        the server which other clients are trying to send them.
+        """
 
-def write():
-    """
-    This function is how the client will create their message and send it
-    to the server to be passed on to the client or clients identified.
-    """
-    # Do this while the application is running
-    while True:
-        # Prompt the user for the message.
-        message = f"{alias}: {input('')}"
-        # Send the message and encode it.
-        client.send(message.encode('utf-8'))
+        # While the application is running do this.
+        while True:
+            # Try and receive messages from the server
+            try:
+                # Receive the message and decode it.
+                message = client.recv(1024).decode('utf-8')
+                # If the message is the alias prompt from the server, then pass the alias.
+                if message == "Enter your alias: ":
+                    # Send the alias to the server
+                    client.send(alias.encode('utf-8'))
+                # Else we are just printing what the server send us as it's a regular message.
+                else:
+                    print(message)
+            # If the receiving message doesn't work, do this.
+            except:
+                # Notify the client that the connection is going to be closed.
+                print("Something went wrong. Closing the connection...")
+                # Close the connection.
+                client.close()
+                # Break out of the loop.
+                break
 
-# Create a thread for receiving messages and start it.
-receive_thread = threading.Thread(target = receive)
-receive_thread.start()
+    def write():
+        """
+        This function is how the client will create their message and send it
+        to the server to be passed on to the client or clients identified.
+        """
+        # Do this while the application is running
+        while True:
+            # Prompt the user for the message.
+            message = f"{alias}: {input('')}"
+            # Send the message and encode it.
+            client.send(message.encode('utf-8'))
 
-# Create a thread for writing/sending messages and start it.
-write_thread = threading.Thread(target = write)
-write_thread.start()
+    # Create a thread for receiving messages and start it.
+    receive_thread = threading.Thread(target = receive)
+    receive_thread.start()
+
+    # Create a thread for writing/sending messages and start it.
+    write_thread = threading.Thread(target = write)
+    write_thread.start()
 
 if __name__ == "__main__":
 	import sys
